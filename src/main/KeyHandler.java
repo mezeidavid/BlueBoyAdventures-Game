b8ada81014476f64a07c172e51362a7ebb887cb7
mezeidavid <127150125+mezeidavid@users.noreply.github.com>
2023-08-03 16:56:15 +0200

    Kód feltöltve az első verzióhoz.

---
+++ src/main/KeyHandler.java
@@ -0,0 +1,66 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+	
+	public boolean upPressed, downPressed, leftPressed, rightPressed, anyKeyPressed;
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		if(e.getKeyCode() == KeyEvent.VK_W) {
+			upPressed = true;
+			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_S) {
+			downPressed = true;
+			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_A) {
+			leftPressed = true;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_D) {
+			rightPressed = true;
+			
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_W) {
+			upPressed = false;
+			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_S) {
+			downPressed = false;
+			
+		}
+		if(e.getKeyCode() == KeyEvent.VK_A) {
+			leftPressed = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_D) {
+			rightPressed = false;
+			
+		}
+		
+	}
+	
+//	public boolean isAnyKeyPressed() {
+//		if(upPressed == true || downPressed == true || leftPressed == true || rightPressed == true) {
+//			anyKeyPressed = true;
+//		}
+//		return anyKeyPressed;
+	} 
+	
+	
+	
+
+
